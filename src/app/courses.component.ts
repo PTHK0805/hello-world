98ce85e32399f29516491fde0a5db9c6e8829dee
PTHK0805 <ptkyaw505@gmail.com>
2022-09-21 16:59:10 +0630

    Directives.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'courses',
-  template: '<h2>Course</h2>'
-  
+  template: `
+    <ul>
+      <li *ngFor="let course of courses">
+        {{course}}
+      </li>
+    </ul>
+  `
 })
 
 export class CoursesComponent {
+  title = 'List of Courses'
+  courses = ['course1', 'course2', 'course3']
+
+  getTitle() {
+    return this.title;
+  }
 }
