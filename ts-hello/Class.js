f7a60cd6acc04b24628b8ec830405da3a705dbdc
PTHK0805 <ptkyaw505@gmail.com>
2022-09-21 10:29:33 +0630

    Properties.

--- ts-hello/Class.js
+++ ts-hello/Class.js
@@ -1,12 +1,18 @@
-var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
+class Point {
+    constructor(_x, _y) {
+        this._x = _x;
+        this._y = _y;
     }
-    Point.prototype.draw = function () {
-        console.log("X: ".concat(this.x, ", Y: ").concat(this.y));
-    };
-    return Point;
-}());
-var point = new Point(2, 1);
+    draw() {
+        console.log(`X: ${this._x}, Y: ${this._y}`);
+    }
+    get x() {
+        return this._x;
+    }
+    set x(value) {
+        this._x = value;
+    }
+}
+const point = new Point(2, 1);
+point.x = 10;
 point.draw();
--- ts-hello/Class.ts
+++ ts-hello/Class.ts
@@ -1,12 +1,21 @@
 class Point {
 
-  constructor(private x?:number,private y?:number) {
+  constructor(private _x?:number,private _y?:number) {
   }
 
   draw() {
-    console.log(`X: ${this.x}, Y: ${this.y}`);
+    console.log(`X: ${this._x}, Y: ${this._y}`);
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value) {
+    this._x = value;
   }
 }
 
 const point = new Point(2, 1);
+point.x = 10;
 point.draw();
