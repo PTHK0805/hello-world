f366ea5d52120482546f433a9e455c4c1a47d604
PTHK0805 <ptkyaw505@gmail.com>
2022-09-21 10:03:58 +0630

    Types.

---
+++ ts-hello/main.ts
@@ -0,0 +1,10 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, 'a', true];
+
+enum Color { Red = 0, Green = 1, Blue = 2 };
+let backgroundColor = Color.Green;
+console.log(backgroundColor);
