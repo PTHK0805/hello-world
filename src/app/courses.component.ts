e4cafc62600e93d08a11bbdc4588a2db4c8ce0da
PTHK0805 <ptkyaw505@gmail.com>
2022-09-22 14:29:37 +0630

    Property Binding.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,12 +10,16 @@ import { Component } from "@angular/core";
         {{course}}
       </li>
     </ul>
+    <!-- Property Binding -->
+    <img [src]="imgUrl"/>
+    <!--<img src="{{imgUrl}}"/>-->
   `
 })
 
 export class CoursesComponent {
   title = 'List of Courses';
   courses: any;
+  imgUrl = "https://pixabay.com/get/ga467ea1c4ee536b6840f3350e8ee9b419bcf4d08cd44a58c67d6ff5087e3205abbc709563564f8fe02fb985632f69d8c72af3f02a9a001e00164185e78f81339_1920.jpg";
 
   constructor(service: CourseService) {
    
