40968c348aa0b0c2d07bc4cec81a5fa5c57871f4
PTHK0805 <ptkyaw505@gmail.com>
2022-09-22 14:40:19 +0630

    Attribute binding.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -13,12 +13,19 @@ import { Component } from "@angular/core";
     <!-- Property Binding -->
     <img [src]="imgUrl"/>
     <!--<img src="{{imgUrl}}"/>-->
-  `
+
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
+  `,
 })
 
 export class CoursesComponent {
   title = 'List of Courses';
   courses: any;
+  colSpan = 2;
   imgUrl = "https://pixabay.com/get/ga467ea1c4ee536b6840f3350e8ee9b419bcf4d08cd44a58c67d6ff5087e3205abbc709563564f8fe02fb985632f69d8c72af3f02a9a001e00164185e78f81339_1920.jpg";
 
   constructor(service: CourseService) {
