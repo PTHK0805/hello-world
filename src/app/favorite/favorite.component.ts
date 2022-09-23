a52fcbda4cbe5c36d047086fa93e4be6c9537c8a
PTHK0805 <ptkyaw505@gmail.com>
2022-09-23 17:00:51 +0630

    Output Properties.

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -7,8 +7,8 @@ import { Component, OnInit, Input} from '@angular/core';
 })
   
 export class FavoriteComponent implements OnInit {
- @Input('is-favorite') isFavorite:any;
-
+  @Input('is-favorite') isFavorite: any;
+  @Output() change = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -16,5 +16,8 @@ export class FavoriteComponent implements OnInit {
 
   onClick() { 
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
+
+  
 }
