2b68cf7cd735849af0f8dc272040f4fe9175443b
PTHK0805 <ptkyaw505@gmail.com>
2022-09-23 16:25:54 +0630

    Aliasing Input Properties.

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
-  inputs: ['isFavorite'],
 })
   
 export class FavoriteComponent implements OnInit {
- isFavorite:any;
+ @Input('is-favorite') isFavorite:any;
 
   constructor() { }
 
