570cf4a4702a131ded634028c295813c8e7763d0
PTHK0805 <ptkyaw505@gmail.com>
2022-09-23 15:51:10 +0630

    Input Properties ("Input Decorator Function")

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
 })
+  
 export class FavoriteComponent implements OnInit {
-  isFavorite: boolean | undefined;
+ @Input() isFavorite:any;
 
   constructor() { }
 
