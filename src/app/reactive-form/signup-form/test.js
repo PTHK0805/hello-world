853ab61c31c2bfe5aa6b4e9bd03e9216633797dd
PTHK0805 <ptkyaw505@gmail.com>
2022-10-12 11:35:30 +0630

    Custom Validator (Unique Name).

---
+++ src/app/reactive-form/signup-form/test.js
@@ -0,0 +1,14 @@
+
+
+users = ['Phyo', 'Thiha', 'Kyaw'];
+result = [];
+
+
+for (let user of users) {
+  let name = 'Thha';
+  if (name === user)
+    result.push(true);
+  else result.push(false);
+}
+console.log(result);
+
--- src/app/reactive-form/signup-form/username.validator.ts
+++ src/app/reactive-form/signup-form/username.validator.ts
@@ -1,10 +1,26 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
+const users = ['Phyo', 'Thiha', 'Kyaw'];
 export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
     if ((control.value as string).includes(' '))
       return { cannotContainSpace: true };
-    
+
+    return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+    let result;
+    for (let user of users) {
+      if ((control.value as string) === user) {
+        result = false;
+        break;
+      }
+
+      else result = true;
+    }
+
+    if (!result) return { shouldBeUnique: true };
     return null;
   }
 }
