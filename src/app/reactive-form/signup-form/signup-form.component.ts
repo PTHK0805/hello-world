8188a7bf24a692f2036fef7c2b7060c4017511b1
PTHK0805 <ptkyaw505@gmail.com>
2022-10-14 08:21:45 +0630

    Changing Spaces: 2 -> 4.

--- src/app/reactive-form/signup-form/signup-form.component.ts
+++ src/app/reactive-form/signup-form/signup-form.component.ts
@@ -3,39 +3,39 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.css']
+    selector: 'app-signup-form',
+    templateUrl: './signup-form.component.html',
+    styleUrls: ['./signup-form.component.css']
 })
 export class SignUpFormComponent implements OnInit {
 
-  form = new FormGroup({
-    username: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace,
-     ], UsernameValidators.shouldBeUnique),
-    
-    password: new FormControl('', Validators.required)
-  });
+    form = new FormGroup({
+        username: new FormControl('', [
+            Validators.required,
+            Validators.minLength(3),
+            UsernameValidators.cannotContainSpace,
+        ], UsernameValidators.shouldBeUnique),
 
-  login() {
-    this.form.setErrors({
-      invalidLogin: true,
-    })
-    console.log(this.form.errors);
-  }
+        password: new FormControl('', Validators.required)
+    });
 
-  get username() {
-    return this.form.get('username');
-  }
+    login() {
+        this.form.setErrors({
+            invalidLogin: true,
+        })
+        console.log(this.form.errors);
+    }
 
-  log(value:any) {
-    console.log(value);
-  }
-  constructor() { }
+    get username() {
+        return this.form.get('username');
+    }
 
-  ngOnInit(): void {
-  }
+    log(value: any) {
+        console.log(value);
+    }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
