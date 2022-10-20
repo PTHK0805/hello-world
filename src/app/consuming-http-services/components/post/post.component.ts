e5846736219ce276f6d518991d0bdc66e0bd5df4
PTHK0805 <ptkyaw505@gmail.com>
2022-10-20 08:48:03 +0630

    Deleting Data.

--- src/app/consuming-http-services/components/post/post.component.ts
+++ src/app/consuming-http-services/components/post/post.component.ts
@@ -37,10 +37,19 @@ export class PostComponent implements OnInit {
   updatePost(post: any, input: HTMLInputElement) {
     this.http.patch(this.url + '/' + post.id, { title: input.value })
       .subscribe(response => {  
-        console.log(response);
+        console.log('Update Post : ',response);
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1, response);
       });
+  }
+
+  deletePost(post: any) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+       let deletedPost = this.posts.splice(index, 1);
+        console.log('Delete Post : ',deletedPost[0]);
+      })
   }
 
   ngOnInit(): void {
