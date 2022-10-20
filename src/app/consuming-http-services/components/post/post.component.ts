9eb1e1418a9da2652d0b7453341d9c67a7451a00
PTHK0805 <ptkyaw505@gmail.com>
2022-10-20 15:56:50 +0630

    Handling Unexpecting Errors.

--- src/app/consuming-http-services/components/post/post.component.ts
+++ src/app/consuming-http-services/components/post/post.component.ts
@@ -12,12 +12,25 @@ export class PostComponent implements OnInit {
   posts: any;
 
   constructor(private http: HttpClient, private postService: PostService) { }
-  
+
   ngOnInit(): void {
-   this.postService.getPosts()
-    .subscribe(response => {
-      this.posts = response;
-    })
+    this.postService.getPosts()
+      //.subscribe(response => {
+      //  this.posts = response;
+      //}, error => {
+      //  alert('An unexpected error occurred');
+      //  console.log(error);
+      //})
+      .subscribe({
+        next: (response) => {
+          this.posts = response;
+        },
+        error: (err: Response) => {
+          alert('An Unexpected error occurred');
+          console.log(err);
+        },
+      })
+
   }
 
   createPost(input: HTMLInputElement) {
@@ -25,34 +38,53 @@ export class PostComponent implements OnInit {
     input.value = '';
 
     this.postService.createPost(post)
-    //this.http.post(this.url, JSON.stringify(post)) /*  <or>  */
-    //this.http.post(this.url, post)
-      .subscribe(response => {
-        post.id = response; /* <or> */
-        //post = response;
-        console.log('Respone : ',response)
-        console.log('Post : ',post);
-        this.posts.splice(0,0 , post);
+      //this.http.post(this.url, JSON.stringify(post)) /*  <or>  */
+      //this.http.post(this.url, post)
+      .subscribe({
+        next: response => {
+          post.id = response; /* <or> */
+          //post = response;
+          console.log('Respone : ', response)
+          console.log('Post : ', post);
+          this.posts.splice(0, 0, post);
+        },
+        error: (err: Response) => {
+          alert('An unexpected error occurred');
+          console.log(err);
+        }
       })
   }
 
   updatePost(post: any, input: HTMLInputElement) {
     //this.http.patch(this.url + '/' + post.id, { title: input.value })
     this.postService.updatePost(post, { title: input.value })
-      .subscribe(response => {  
-        console.log('Update Post : ',response);
-        let index = this.posts.indexOf(post);
-        this.posts.splice(index, 1, response);
+      .subscribe({
+        next: response => {
+          console.log('Update Post : ', response);
+          let index = this.posts.indexOf(post);
+          this.posts.splice(index, 1, response);
+        },
+        error: (err: Response) => {
+          alert('An unexpected error occurred');
+          console.log(err);
+        }
       });
   }
 
   deletePost(post: any) {
     //this.http.delete(this.url + '/' + post.id)
     this.postService.deletePost(post.id)
-      .subscribe(response => {
-        let index = this.posts.indexOf(post);
-       let deletedPost = this.posts.splice(index, 1);
-        console.log('Delete Post : ',deletedPost[0]);
+      .subscribe({
+        next: response => {
+          let index = this.posts.indexOf(post);
+          let deletedPost = this.posts.splice(index, 1);
+          console.log('Delete Post : ', deletedPost[0]);
+        },
+        error: (err: Response) => {
+          alert('An unexpected error occurred');
+          console.log(err);
+
+        }
       })
   }
 
